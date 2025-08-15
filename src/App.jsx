377534e276918a0ab38a678bb10dd72e1424a523
sakshyam <sakshyamshrestha@111gmail.com>
2025-08-15 17:34:58 +0545

    first

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import TexasCampusPlatform from './components/TexasCampusPlatform';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import ProfilePage from './components/ProfilePage';
+import LandingPage from './components/LandingPage';
 
 const App = () => {
   return (
     <ThemeProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<TexasCampusPlatform />} />
           <Route path="/profile" element={<ProfilePage />} />
