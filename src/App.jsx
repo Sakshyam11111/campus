c844ba6088e0d8a3467d032f08da511a22b8c4d4
sakshyam <sakshyamshrestha@111gmail.com>
2025-08-15 11:26:06 +0545

    ffF

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-// App.jsx (Wraps app with GoogleOAuthProvider for Google auth)
+// App.jsx (Updated to use Router)
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import TexasCampusPlatform from './components/TexasCampusPlatform';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
 const App = () => {
   return (
-    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID_HERE"> {/* Replace with your actual Client ID */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<TexasCampusPlatform />} />
-        </Routes>
-      </Router>
-    </GoogleOAuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<TexasCampusPlatform />} />
+      </Routes>
+    </Router>
   );
 };
 
